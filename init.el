bc24e67ffbd36372ba990e7218755fec99ac5404
jennifersmith <jennifer@home>
2014-12-20 11:12:34 +1100

    Rainbow-mode works now I have gnu-elpa in repos

--- init.el
+++ init.el
@@ -6,8 +6,7 @@
 ;;
 ;; http://orgmode.org/worg/org-contrib/babel/intro.html
 (require 'package)
-(setq package-archives '(;; ("gnu" . "http://elpa.gnu.org/packages/")
-			 ;; ("marmalade" . "http://marmalade-repo.org/packages/")
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("org" . "http://orgmode.org/elpa/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
 
