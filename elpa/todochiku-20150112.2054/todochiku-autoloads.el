ef95e4dc7ad494be49714ea2061b276e2fa7a266
jennifersmith <jennifer@home>
2015-02-13 19:25:05 +1100

    Added more packages

---
+++ elpa/todochiku-20150112.2054/todochiku-autoloads.el
@@ -0,0 +1,15 @@
+;;; todochiku-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("todochiku.el") (21718 47529 261213 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; todochiku-autoloads.el ends here
